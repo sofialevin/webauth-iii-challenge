c5245af8a38cbc2238c1412922789349f366e82c
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-18 12:03:45 -0800

    Completed and ran migration.

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/auth.db3',
+      filename: './data/users.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
