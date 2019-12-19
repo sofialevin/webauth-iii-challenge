899295f11f129dfcd1a89b6706e4e4d9f1e7e9aa
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-18 20:14:04 -0800

    Completed user model.

--- users/user-model.js
+++ users/user-model.js
@@ -1,1 +1,31 @@
 const db = require('../data/dbConfig.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+function find() {
+  return db('users').select('id', 'username', 'role');
+}
+
+function findBy(filter) {
+  return db('users').where(filter).select('id', 'username', 'role');
+}
+
+function add(user) {
+  return db("users")
+    .insert(user)
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
