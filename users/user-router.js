0e01608f8632743cf3ba82caf998e8fd176d375f
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-18 22:37:42 -0800

    Completed user router.

--- users/user-router.js
+++ users/user-router.js
@@ -2,4 +2,14 @@ const router = require('express').Router();
 
 const Users = require('./user-model.js');
 
+const restricted = require('../auth/restricted-middleware.js');
+
+router.get('/', restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+})
+
 module.exports = router;
