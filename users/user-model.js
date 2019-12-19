0be3193f16eedb65cf5133dd52e7bcfccf845e25
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-18 22:26:11 -0800

    Small change on user model.

--- users/user-model.js
+++ users/user-model.js
@@ -19,10 +19,13 @@ function findBy(filter) {
     .first();
 }
 
-async function add(user) {
-  const [id] = await db('users').insert(user).returning('id');
-
-  return findById(id);
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
 }
 
 function findById(id) {
